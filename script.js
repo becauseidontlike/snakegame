44d39a597054417e87c418dbd6e84cbad1343fc5
becauseidontlike <120679852+becauseidontlike@users.noreply.github.com>
2024-02-11 16:48:19 +0100

    moving and growing option

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 "use strict";
 
 const board = document.getElementById("game-board");
+const gridSize = 20;
 let snake = [{ x: 10, y: 10 }];
+let food = generateFood();
+let direction = "right";
+let gameInterval;
+let gameSpeedDelay = 200;
+let gameStarted = false;
 
 function draw() {
   board.innerHTML = "";
   drawSnake();
+  drawFood();
 }
 
 function drawSnake() {
@@ -27,4 +34,54 @@ function setPosition(element, position) {
   element.style.gridRow = position.y;
 }
 
-draw();
+function drawFood() {
+  const foodElement = createGameElement("div", "food");
+  setPosition(foodElement, food);
+  board.appendChild(foodElement);
+}
+
+function generateFood() {
+  const x = Math.floor(Math.random() * gridSize) + 1;
+  const y = Math.floor(Math.random() * gridSize) + 1;
+  return { x, y };
+}
+
+function move() {
+  const head = { ...snake[0] };
+  switch (direction) {
+    case "right":
+      head.x++;
+      break;
+    case "down":
+      head.y++;
+      break;
+    case "up":
+      head.y--;
+      break;
+    case "left":
+      head.x--;
+      break;
+  }
+  snake.unshift(head);
+  // snake.pop();
+
+  if (head.x === food.x && head.y === food.y) {
+    food = generateFood();
+    clearInterval();
+    gameInterval = setInterval(() => {
+      move();
+      draw();
+    }, gameSpeedDelay);
+  } else {
+    snake.pop();
+  }
+}
+
+// setInterval(() => {
+//   move();
+//   draw();
+// }, 200);
+
+function startGame() {
+  gameStarted = true;
+}
